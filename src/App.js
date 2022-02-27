f20a3ac5fe29dc01e7cdc087f43bfb2107249107
Tanay Raj <tanay.raj76@gmail.com>
2022-02-28 00:31:28 +0530

    dynamic line chart add

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [rData, setData] = useState([{ lines: 0 }]);
+
+  useEffect(() => {
+    const eventSource = new EventSource(`http://localhost:8080/`);
+    eventSource.onmessage = (e) => {
+      console.log(e.data);
+      setData((oldArray) => [...oldArray, { lines: parseInt(e.data) }]);
+
+      console.log(rData);
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <h2>Change in LOC</h2>
+      <ResponsiveContainer width="90%" aspect={3}>
+        <LineChart
+          width={500}
+          height={300}
+          data={rData}
+          margin={{
+            top: 15,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <CartesianGrid horizontal="true" vertical="" stroke="#243240" />
+          <XAxis dataKey="lines" tick={{ fill: "#fff" }} />
+          <YAxis tick={{ fill: "#fff" }} />
+          <Tooltip
+            contentStyle={{ backgroundColor: "#8884d8", color: "#fff" }}
+            itemStyle={{ color: "#fff" }}
+            cursor={false}
+          />
+          <Line
+            type="monotone"
+            dataKey="lines"
+            stroke="#8884d8"
+            strokeWidth="5"
+            dot={{ fill: "#2e4355", stroke: "#8884d8", strokeWidth: 2, r: 5 }}
+            activeDot={{
+              fill: "#2e4355",
+              stroke: "#8884d8",
+              strokeWidth: 5,
+              r: 10,
+            }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </>
   );
 }
 
