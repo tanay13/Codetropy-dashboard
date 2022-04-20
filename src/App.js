4bcb1e5d1c479854b403fb331b2b822e276ced63
Tanay Raj <tanay.raj76@gmail.com>
2022-04-20 12:28:21 +0530

    different route for diff team

--- src/App.js
+++ src/App.js
@@ -1,71 +1,19 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  ResponsiveContainer,
-} from "recharts";
-import { useEffect, useState } from "react";
+import Dashboard from "./page/dashboard";
+import Home from "./page/Home";
 
 function App() {
-  const [rData, setData] = useState([{ lines: 0 }]);
-
-  useEffect(() => {
-    const eventSource = new EventSource(`http://localhost:8080/`);
-    eventSource.onmessage = (e) => {
-      console.log(e.data);
-      setData((oldArray) => [...oldArray, { lines: parseInt(e.data) }]);
-
-      console.log(rData);
-    };
-
-    return () => {
-      eventSource.close();
-    };
-  }, []);
-
   return (
-    <>
-      <h2>Change in LOC</h2>
-      <ResponsiveContainer width="90%" aspect={3}>
-        <LineChart
-          width={500}
-          height={300}
-          data={rData}
-          margin={{
-            top: 15,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid horizontal="true" vertical="" stroke="#243240" />
-          <XAxis dataKey="lines" tick={{ fill: "#fff" }} />
-          <YAxis tick={{ fill: "#fff" }} />
-          <Tooltip
-            contentStyle={{ backgroundColor: "#8884d8", color: "#fff" }}
-            itemStyle={{ color: "#fff" }}
-            cursor={false}
-          />
-          <Line
-            type="monotone"
-            dataKey="lines"
-            stroke="#8884d8"
-            strokeWidth="5"
-            dot={{ fill: "#2e4355", stroke: "#8884d8", strokeWidth: 2, r: 5 }}
-            activeDot={{
-              fill: "#2e4355",
-              stroke: "#8884d8",
-              strokeWidth: 5,
-              r: 10,
-            }}
-          />
-        </LineChart>
-      </ResponsiveContainer>
-    </>
+    // basename="/tut" - prop passed to BrowserRouter so that it takes us to the page /tut/routes.
+    <BrowserRouter>
+      <div className="App"></div>
+      <Switch>
+        {/* switch goes to the path which matches first.Exact is used so that routing is done exactly as the path mentioned */}
+        <Route path="/" component={Home} exact />
+        <Route path="/dashboard/:teamname" component={Dashboard} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/page/dashboard.js
@@ -0,0 +1,74 @@
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+function Dashboard() {
+  const [rData, setData] = useState([{ lines: 0 }]);
+  let { teamname } = useParams();
+  useEffect(() => {
+    const eventSource = new EventSource(
+      `http://localhost:8080/stream/${teamname}`
+    );
+    eventSource.onmessage = (e) => {
+      console.log(e.data);
+      setData((oldArray) => [...oldArray, { lines: parseInt(e.data) }]);
+
+      console.log(rData);
+    };
+
+    return () => {
+      eventSource.close();
+    };
+  }, []);
+
+  return (
+    <>
+      <h2>Change in LOC</h2>
+      <ResponsiveContainer width="90%" aspect={3}>
+        <LineChart
+          width={500}
+          height={300}
+          data={rData}
+          margin={{
+            top: 15,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid horizontal="true" vertical="" stroke="#243240" />
+          <XAxis dataKey="lines" tick={{ fill: "#fff" }} />
+          <YAxis tick={{ fill: "#fff" }} />
+          <Tooltip
+            contentStyle={{ backgroundColor: "#8884d8", color: "#fff" }}
+            itemStyle={{ color: "#fff" }}
+            cursor={false}
+          />
+          <Line
+            type="monotone"
+            dataKey="lines"
+            stroke="#8884d8"
+            strokeWidth="5"
+            dot={{ fill: "#2e4355", stroke: "#8884d8", strokeWidth: 2, r: 5 }}
+            activeDot={{
+              fill: "#2e4355",
+              stroke: "#8884d8",
+              strokeWidth: 5,
+              r: 10,
+            }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
+    </>
+  );
+}
+
+export default Dashboard;
